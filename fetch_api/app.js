1b734ebffce7171eb9f5cd3c0285796b2dbfdc91
shalr-20 <shalini.roy2035@gmail.com>
2024-08-23 00:33:44 +0530

    app

--- fetch_api/app.js
+++ fetch_api/app.js
@@ -1,2 +1,8 @@
 const URL="https://cat-fact.herokuapp.com/facts";
 
+const getFacts=async()=>{
+    console.log("Getting data...");
+    let response=await fetch(URL);
+    console.log(response);
+    
+}
